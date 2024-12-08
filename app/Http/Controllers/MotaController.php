8db229c50b7042bb662a3e916782128fe2209013
Paulino Fonseca <paulinofonsecass@gmail.com>
2024-12-08 19:13:50 +0100

    feat: create a mota model

---
+++ app/Http/Controllers/MotaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreMotaRequest;
+use App\Http\Requests\UpdateMotaRequest;
+use App\Models\Mota;
+
+class MotaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMotaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Mota $mota)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Mota $mota)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMotaRequest $request, Mota $mota)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Mota $mota)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_12_08_181247_create_motas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     * id (integer): Identificador único.
+     * nome (string): Nome ou modelo da motorizada.
+     * preco (decimal): Preço da motorizada.
+     * descricao (text): Descrição detalhada da motorizada.
+     * disponivel (boolean): Indica se a motorizada está disponível para reserva.
+     * imagem (string): Caminho para a imagem da motorizada.
+     */
+    public function up(): void
+    {
+        Schema::create('motas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->decimal('preco');
+            $table->text('descricao');
+            $table->boolean('disponivel');
+            $table->string('imagem_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('motas');
+    }
+};
