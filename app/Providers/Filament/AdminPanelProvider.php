223b769af51ddf549a34f35d7c56791c84e1098f
Paulino Fonseca <paulinofonsecass@gmail.com>
2025-02-06 17:44:09 +0100

    feat: adiciona recurso de marca

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Resources\MarcaResource;
 use App\Filament\Resources\MotaResource;
 use App\Filament\Resources\ReservaResource;
+use App\Models\Marca;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -42,6 +44,7 @@ class AdminPanelProvider extends PanelProvider
                             ->url('/admin'),
                         ...MotaResource::getNavigationItems(),
                         ...ReservaResource::getNavigationItems(),
+                        ...MarcaResource::getNavigationItems(),
 
                     ]);
             })
